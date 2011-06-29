60b5c4dfe0badd71d5bb93f3431e320d77a9461c
Ewald Zietsman <ewald@siyavula.org.za>
2011-06-29 18:48:37 +0200

    Handle Sword Deposit

--- rhaptos/swordservice/plone/browser/sword.py
+++ rhaptos/swordservice/plone/browser/sword.py
@@ -1,16 +1,38 @@
+import zipfile
+from cStringIO import StringIO
+
+
 from Products.Five import BrowserView
 from Products.CMFCore.utils import getToolByName
 
 
-class SWORDService(BrowserView):
-    """SWORD Service for plone   """
+class SWORDServiceDocument(BrowserView):
+    """ SWORD Service for plone   """
     
     def collections(self):
         """Return all folders we have access to as collection targets"""
         pc = getToolByName(self.context, "portal_catalog")
-        return pc(portal_type='Folder', allowedRolesAndUsers=['Contributor'])
+        return pc(portal_type='Folder', allowedRolesAndUsers=['contributor'])
         
         
         
         
     
+class SWORDDeposit(BrowserView):
+    """ Handle SWORD deposit """
+    
+    def __call__(self):
+        """  """
+        #import pdb; pdb.set_trace()
+        
+        if self.request.getHeader('content-type') == 'application/zip':
+            bodyfile = StringIO(self.request.get('BODY'))
+            zf = zipfile.ZipFile(bodyfile)
+            
+            #import pdb; pdb.set_trace() 
+            for filepath in zf.namelist():
+                fileobj = zf.read(filepath)
+                
+            
+            
+            
