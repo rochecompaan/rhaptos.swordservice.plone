6e6015e865e4fa8398d4dc2b417e8eafd34ef31a
Roché Compaan <roche@upfrontsystems.co.za>
2011-07-03 18:44:41 +0200

    implement custom traversal for SWORD service that consumes subpath and use a single browser view

--- rhaptos/swordservice/plone/browser/sword.py
+++ rhaptos/swordservice/plone/browser/sword.py
@@ -1,38 +1,54 @@
 import zipfile
 from cStringIO import StringIO
 
+from zope.interface import Interface, implements
+from zope.publisher.interfaces.http import IHTTPRequest
+from zope.component import adapts, getMultiAdapter
 
+from ZPublisher.BaseRequest import DefaultPublishTraverse
 from Products.Five import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
 
+class ISWORDService(Interface):
+    """ Marker interface for SWORD service """
 
-class SWORDServiceDocument(BrowserView):
-    """ SWORD Service for plone   """
-    
-    def collections(self):
-        """Return all folders we have access to as collection targets"""
-        pc = getToolByName(self.context, "portal_catalog")
-        return pc(portal_type='Folder', allowedRolesAndUsers=['contributor'])
-        
-        
-        
-        
-    
-class SWORDDeposit(BrowserView):
-    """ Handle SWORD deposit """
-    
+class SWORDService(BrowserView):
+
+    implements(ISWORDService)
+
+    servicedocument = ViewPageTemplateFile('servicedocument.pt')
+
     def __call__(self):
-        """  """
-        #import pdb; pdb.set_trace()
-        
         if self.request.getHeader('content-type') == 'application/zip':
             bodyfile = StringIO(self.request.get('BODY'))
             zf = zipfile.ZipFile(bodyfile)
             
-            #import pdb; pdb.set_trace() 
             for filepath in zf.namelist():
                 fileobj = zf.read(filepath)
-                
-            
-            
-            
+
+    def collections(self):
+        """Return all folders we have access to as collection targets"""
+        pc = getToolByName(self.context, "portal_catalog")
+        return pc(portal_type='Folder', allowedRolesAndUsers=['contributor'])
+    
+
+class SWORDTraversel(DefaultPublishTraverse):
+    """ Implement custom traversal for ISWORDService to allow the use
+        of "sword" as a namespace in our path and use the sub path to
+        determine the resource we want or action required.
+
+        Basically this gives us nice RESTful URLs eg:
+
+            <Plone Site>/sword/service-document
+            <Folder>/sword
+    """
+
+    adapts(ISWORDService, IHTTPRequest)
+
+    def publishTraverse(self, request, name):
+        if name == 'service-document':
+            return self.context.servicedocument()
+        else:
+            return super(SWORDTraversel, self).publishTraverse(request, name)
+
