92ed33f7d599e7d5f6e7e757144dfc3bacfb8246
Izak Burger <isburger@gmail.com>
2011-07-27 23:35:08 +0200

    add content using plone's content type registry

--- rhaptos/swordservice/plone/browser/sword.py
+++ rhaptos/swordservice/plone/browser/sword.py
@@ -1,11 +1,15 @@
+import sys
 import zipfile
 from cStringIO import StringIO
 
 from zope.interface import Interface, implements
 from zope.publisher.interfaces.http import IHTTPRequest
 from zope.component import adapts, getMultiAdapter
+from zope.contenttype import guess_content_type
 
+from Acquisition import aq_inner
 from ZPublisher.BaseRequest import DefaultPublishTraverse
+from zExceptions import Unauthorized
 from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFCore.utils import getToolByName
@@ -20,12 +24,56 @@ class SWORDService(BrowserView):
     servicedocument = ViewPageTemplateFile('servicedocument.pt')
 
     def __call__(self):
-        if self.request.getHeader('content-type') == 'application/zip':
-            bodyfile = StringIO(self.request.get('BODY'))
-            zf = zipfile.ZipFile(bodyfile)
-            
-            for filepath in zf.namelist():
-                fileobj = zf.read(filepath)
+        assert self.request.method == 'POST',"Method %s not supported" % (
+            self.request.method)
+        context = aq_inner(self.context)
+        content_type = self.request.getHeader('content-type')
+        disposition = self.request.getHeader('content-disposition')
+        filename = None
+        if disposition is not None:
+            try:
+                filename = [x for x in disposition.split(';') \
+                    if x.strip().startswith('filename=')][0][10:]
+            except IndexError:
+                pass
+
+        body = self.request.get('BODY')
+        if content_type is None and not (filename is None and body is None):
+            content_type, encoding = guess_content_type(filename, body)
+
+        # If we couldn't guess either, assume its octet-stream
+        if content_type is None:
+            content_type = "application/octet-stream"
+
+        # If no filename, make one up
+        if filename is None:
+            filename = context.generateUniqueId(
+                type_name=content_type.replace('/', '_'))
+
+        # PUT_factory is implemented by PortalFolder
+        factory = getattr(context, 'PUT_factory', None)
+        assert factory is not None, "Parent does not implement PUT_factory"
+
+        ob = factory(filename, content_type, body)
+
+        # Persist ob in context
+        try:
+            context._verifyObjectPaste(ob.__of__(context), 0)
+        except CopyError:
+             sMsg = 'Unable to create object of class %s in %s: %s' % \
+                    (ob.__class__, repr(context), sys.exc_info()[1],)
+             raise Unauthorized, sMsg
+
+        context._setObject(filename, ob)
+        ob = context._getOb(filename)
+        ob.PUT(self.request, self.request.response)
+        ob.setTitle(filename)
+        ob.reindexObject(idxs='Title')
+
+        self.request.response.setHeader('Location', ob.absolute_url())
+        self.request.response.setStatus(201)
+        self.request.response.setBody('')
+        return None
 
     def collections(self):
         """Return all folders we have access to as collection targets"""
